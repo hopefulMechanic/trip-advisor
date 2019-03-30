47a65f05e24df27482cf21c21dc39f066578bb2b
Dawid Taborski <taborski.dawid@gmail.com>
2019-03-30 13:36:07 +0100

    init build frontend and backed

---
+++ backend/src/test/java/studies/project/tripadvisor/TripAdvisorApplicationTests.java
@@ -0,0 +1,16 @@
+package studies.project.tripadvisor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TripAdvisorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
