3c3ef7ed32064e85cd1be26570f6527aac1dff1a
Kamil K Kleczek <euvkkle1@amway.com>
2019-04-04 11:06:04 +0200

    proof of concept for place factory

---
+++ backend/src/main/java/studies/project/tripadvisor/core/place/Place.java
@@ -0,0 +1,25 @@
+package studies.project.tripadvisor.core.place;
+
+public class Place {
+    private String name;
+    private String type;
+    // #TODO add address
+    //private Address address
+    // # TODO add comments
+    //private CommentsList commentsList;
+    // #TODO add score
+    //private ScoresList scoresList;
+    // #TODO add categories
+    // private CategoriesList categoriesList;
+    private float score;
+
+//    public CommentsList addComment(String comment) {
+//        return null;
+//    }
+
+//    public score addScore(float score){
+//        return null;
+//    }
+
+
+}
---
+++ backend/src/main/java/studies/project/tripadvisor/core/place/Public.java
@@ -0,0 +1,7 @@
+package studies.project.tripadvisor.core.place;
+
+public class Public extends Place {
+    private boolean isEntryFee;
+    private float entryFee;
+
+}
