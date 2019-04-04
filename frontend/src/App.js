21507a7b1f5eaf5c0be89a8c32b3022fb73fc492
Dawid Taborski <taborski.dawid@gmail.com>
2019-04-04 17:08:14 +0200

    [Register] dodanie komponentow dashboard, login-page, register-form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import "./App.css";
+import LoginPage from "./components/LoginPage/LoginPage";
+import Dashboard from "./components/Dashboard/Dashboard";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
+import { LoginService } from "./service/LoginService";
+import Loader from "./components/Loader/Loader";
 
 class App extends Component {
+  state = {
+    isAuthenticated: false
+  };
+
+  componentDidMount() {
+    this.isUserLoggedin();
+  }
+
+  isUserLoggedin() {
+    LoginService.isUserAutheticated().then(res =>
+      setState({ isAuthenticated: res, loading: false })
+    );
+  }
+
   render() {
+    const { isAuthenticated, loading } = this.state;
+    const redirect = isAuthenticated ? (
+      <Redirect to="/dashboard" />
+    ) : (
+      <Redirect to="/login" />
+    );
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <Router>
+            <main>
+              {(!loading && (
+                <Switch>
+                  <Route path="/login" component={LoginPage} />
+                  <Route exect path="/dashboard" component={Dashboard} />
+                  <Route exect path="/register" component={RegisterForm} />
+                  {redirect}
+                </Switch>
+              )) || <Loader />}
+            </main>
+          </Router>
+        </div>
       </div>
     );
   }
