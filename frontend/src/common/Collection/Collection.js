b3f26763e8a65dbef6b8138a80a95c504f5c2fed
Dawid Taborski <taborski.dawid@gmail.com>
2019-04-09 15:37:23 +0200

    css fixes

--- frontend/src/common/Collection/Collection.js
+++ frontend/src/common/Collection/Collection.js
@@ -4,13 +4,15 @@ const Collection = ({ list, onDoubleClick }) => {
   return (
     <ul class="list-group w-100">
       {list.map(el => (
-        <li
+        <button
+          type="button"
           key={el.id}
-          class="list-group-item"
+          class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
           onClick={() => onDoubleClick && onDoubleClick(el.id)}
         >
           {el.content}
-        </li>
+          <span class="badge badge-primary badge-pill">2</span>
+        </button>
       ))}
     </ul>
   );
