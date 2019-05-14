75d0e13a5632637e2ce634daae830a7598ec303c
Dominik <37372682+hopefulmechanic@users.noreply.github.com>
2019-05-14 21:29:02 +0200

    fix endpoint address mapping

--- backend/src/main/java/studies/project/tripadvisor/controller/NotificationController.java
+++ backend/src/main/java/studies/project/tripadvisor/controller/NotificationController.java
@@ -12,45 +12,45 @@ import studies.project.tripadvisor.service.NotificationService;
 @Slf4j
 @CrossOrigin()
 @RestController
-@RequestMapping("api/notification")
+@RequestMapping("api")
 public class NotificationController {
 
     @Autowired
     NotificationService notificationService;
 
-    @GetMapping("{userId}")
+    @GetMapping("/notification/{userId}")
     public ResponseEntity retrieveMessages(@PathVariable(name = "userId") Long userId) throws ElementNotFoundException {
         return new ResponseEntity(notificationService.retrieveMessages(userId), HttpStatus.OK);
     }
 
-    @PostMapping("notify/{placeId}")
+    @PostMapping("/notification/notify/{placeId}")
     public ResponseEntity notifyObservers(@RequestBody String message,
                                           @PathVariable(name = "placeId") Long placeId) {
         notificationService.notifyObservers(placeId, message);
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @PostMapping("add/{placeId}/{userId}")
+    @PostMapping("/notification/add/{placeId}/{userId}")
     public ResponseEntity addObserver(@PathVariable(name = "placeId") Long placeId,
                                       @PathVariable(name = "userId") Long userId) throws ElementNotFoundException {
         notificationService.addObserver(placeId, userId);
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @DeleteMapping("remove/{placeId}/{userId}")
+    @DeleteMapping("/notification/{placeId}/{userId}")
     public ResponseEntity removeObserver(@PathVariable(name = "placeId") Long placeId,
                                          @PathVariable(name = "userId") Long userId) throws ElementNotFoundException {
         notificationService.removeObserver(placeId, userId);
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @DeleteMapping("deleteMessages/{userId}")
+    @DeleteMapping("/user/{userId}/messages")
     public ResponseEntity deleteMessages(@PathVariable(name = "userId") Long userId) throws ElementNotFoundException {
         notificationService.deleteMessages(userId);
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @GetMapping("check/{placeId}/{userId}")
+    @GetMapping("/notification/check/{placeId}/{userId}")
     public ResponseEntity checkIfObserve(@PathVariable(name = "placeId") Long placeId,
                                          @PathVariable(name = "userId") Long userId) throws ElementNotFoundException {
         if (notificationService.checkIfObserve(placeId, userId)) {
