b016ee75041b7ea0cec4d0af101d526538955914
Kamil K Kleczek <euvkkle1@amway.com>
2019-05-18 12:02:36 +0200

    unit test

--- backend/src/main/java/studies/project/tripadvisor/entity/Comment.java
+++ backend/src/main/java/studies/project/tripadvisor/entity/Comment.java
@@ -3,6 +3,7 @@ package studies.project.tripadvisor.entity;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,7 @@ import javax.persistence.*;
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "COMMENT")
+@AllArgsConstructor
 public class Comment {
 
     @Id
